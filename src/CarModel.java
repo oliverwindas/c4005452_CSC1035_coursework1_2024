cb05049cffa5f4f7982b0a07259f759211c8d35a
protocolpenguin <59795922+protocolpenguin@users.noreply.github.com>
2025-02-07 16:55:11 +0000

    Create relevant fields as per described byy the brief

--- src/CarModel.java
+++ src/CarModel.java
@@ -1,2 +1,11 @@
-public class CarModel {
+import java.util.Objects;
+
+public class CarModel
+{
+    private String name;
+    private double weight;
+    private double salesPrice;
+    private int numberSold;
+    private String type; // "either hatchback, saloon or estate" as per the brief
+
 }
