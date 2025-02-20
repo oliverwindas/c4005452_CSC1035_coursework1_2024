2cd0be1d1a78ba36abab84cb11b8f6f68e338c47
protocolpenguin <59795922+protocolpenguin@users.noreply.github.com>
2025-02-20 22:22:37 +0000

    Print all the menu options mention in the brief, using a while loop that will then changfe if exit is selected.

--- src/ReportingIO.java
+++ src/ReportingIO.java
@@ -5,5 +5,19 @@ public class ReportingIO
     private static Reporting reporting = new Reporting();
     private static Scanner scanner = new Scanner(System.in);
 
+    public static void main(String[] args)
+    {
+        boolean running = true;
 
+        while (running) {
+            System.out.println("Menu:");
+            System.out.println("1. Enter Manufacturer Data");
+            System.out.println("2. Enter Car Model Data");
+            System.out.println("3. List All Manufacturers");
+            System.out.println("4. List All Car Models by Manufacturer");
+            System.out.println("5. Provide Reporting Statistics");
+            System.out.println("6. Exit");
+            System.out.print("Choose an option: ");
+        }
+    }
 }
