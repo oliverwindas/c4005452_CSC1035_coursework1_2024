bb88464b4890a83f0ebefa6fe2271d25b44c9d3b
protocolpenguin <59795922+protocolpenguin@users.noreply.github.com>
2025-02-20 23:01:02 +0000

    Revert "Gets the HIghest Revenue Manufacturer dependant on car type."
    This reverts commit 97db91d52ae7fa32453175c2b62a2f7eb2ebd13a.

--- src/ReportingIO.java
+++ src/ReportingIO.java
@@ -123,20 +123,7 @@ public class ReportingIO
 
     private static void provideReportingStatistics()
     {
-        System.out.println("Reporting Statistics:");
 
-        // Manufacturer with the largest revenue for cars sold of a given type
-        System.out.print("Enter car type for revenue statistics (hatchback, saloon, estate): ");
-        String type = scanner.nextLine();
-        Manufacturer highestRevenueManufacturer = reporting.getManufacturerWithLargestRevenue(type);
-        if (highestRevenueManufacturer != null)
-        {
-            System.out.println("Manufacturer with the largest revenue for " + type + "'s : " + highestRevenueManufacturer.getName());
-        }
-        else
-        {
-            System.out.println("No data available for the specified type.");
-        }
     }
 
     private static Manufacturer findManufacturer(String name)
